00e6ee12c6d372e6d9721f1590bc9dbe2f894508
Your Name <you@example.comnam>
2022-06-14 20:38:19 +0530

    minor changes

--- qr.js
+++ qr.js
@@ -1,8 +1,10 @@
-
-let close = document.getElementById('close')
+let close = document.getElementById("close");
 
 function docReady(fn) {
-    if (document.readyState === "complete" || document.readyState === "interactive") {
+    if (
+        document.readyState === "complete" ||
+        document.readyState === "interactive"
+    ) {
         setTimeout(fn, 1);
     } else {
         document.addEventListener("DOMContentLoaded", fn);
@@ -11,60 +13,65 @@ function docReady(fn) {
 
 docReady(doScan);
 
-let scanBtn = document.getElementById('gallery')
+let scanBtn = document.getElementById("gallery");
+scanBtn.addEventListener("click", toggleScan);
 
-scanBtn.addEventListener('click', toggleScan)
-// close.addEventListener('click', goHome)
-
-let isScan = true
+let isScan = true;
 
 function toggleScan() {
-    let start = document.querySelector('span:nth-child(2)>button:nth-child(1)')
-    let stop = document.querySelector('span:nth-child(2)>button:nth-child(2)')
-    console.log('start:', start)
-    console.log('stop:', stop)
+    let start = document.querySelector("span:nth-child(2)>button:nth-child(1)");
+    let stop = document.querySelector("span:nth-child(2)>button:nth-child(2)");
+    console.log("start:", start);
+    console.log("stop:", stop);
     if (isScan) {
-        stop.click()
-        isScan = false
+        stop.click();
+        isScan = false;
     } else {
-        start.click()
-        isScan = true
+        start.click();
+        isScan = true;
     }
-    console.log('stopping')
-    html5QrCode.stop()
+    let select = document.getElementById('qr-reader__camera_selection')
+    console.log('select before change', select)
+    let cam2 = document.querySelectorAll('option')[1]
+    cam2.setAttribute('selected','')
+    console.log('cam2:', cam2)
+    select.value = cam2.value
+    console.log('select changed', select)
 }
 
 function goHome() {
-    window.location.href = '/index.html'
+    window.location.href = "/index.html";
 }
 
 function formateQr(str) {
-    let str1 = str.split('pn=')[1].split('&')[0].replaceAll('%20', ' ')
-    let str2 = str.split('pa=')[1].split('&')[0].replaceAll('%20', ' ')
-    return [str1, str2]
+    let str1 = str.split("pn=")[1].split("&")[0].replaceAll("%20", " ");
+    let str2 = str.split("pa=")[1].split("&")[0].replaceAll("%20", " ");
+    return [str1, str2];
 }
 
 function doScan() {
     var qrScaner = new Html5QrcodeScanner("qr-reader", { fps: 10, qrbox: 500 });
-    let inputData = {}
+    let inputData = {};
     function onScanSuccess(decodedText, decodedResult) {
-        let formateData = formateQr('\n' + decodedText)
-        inputData.merch = formateData[0]
-        inputData.name = formateData[1]
-        localStorage.setItem('data', JSON.stringify(inputData))
-        qrScaner.clear()
-        window.location.href = '/index.html'
+        let formateData = formateQr("\n" + decodedText);
+        inputData.merch = formateData[0];
+        inputData.name = formateData[1];
+        localStorage.setItem("data", JSON.stringify(inputData));
+        qrScaner.clear();
+        window.location.href = "/index.html";
+
     }
-    function onScanError(err) {
-    }
+    function onScanError(err) { }
     qrScaner.render(onScanSuccess, onScanError);
 }
 
-window.onload = ()=>{
-    let camaccess = get('#qr-reader__camera_permission_button')
-    
-    if(camaccess){
-        camaccess.click()
+window.onload = () => {
+    let camaccess = get("#qr-reader__camera_permission_button");
+
+    if (camaccess) {
+        camaccess.click();
     }
-}
+
+};
+
 
